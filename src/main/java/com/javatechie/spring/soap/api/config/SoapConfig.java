89b263648df42c8bf2d5fe9bb3f6b58a3537b542
RahulDNair <rahuldiwanair@gmail.com>
2023-10-29 21:17:59 +0530

    first commit to client

---
+++ src/main/java/com/javatechie/spring/soap/api/config/SoapConfig.java
@@ -0,0 +1,18 @@
+package com.javatechie.spring.soap.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this is the package name specified in the <generatePackage> specified in pom.xml
+        marshaller.setPackagesToScan("com.javatechie.spring.soap.api.loaneligibility");
+        return marshaller;
+    }
+
+}
